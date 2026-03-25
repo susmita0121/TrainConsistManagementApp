7af4bc749b1e90ce3733ceb7fcc64dfe6c6e2557
susmita0121 <sushh2123@gmail.com>
2026-03-25 09:50:04 +0530

    UC5

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -1,49 +1,38 @@
+import java.util.LinkedHashSet;
 
-import java.util.LinkedList;
 
 
 public class TrainConsistManagementApp {
+
 
         public static void main(String[] args) {
 
 
             System.out.println("====================================");
-            System.out.println("=== UC4 - Maintain Ordered Bogie Consist ===");
+            System.out.println("=== UC5 - Preserve Insertion Order of Bogies ===");
             System.out.println("====================================");
 
 
-            LinkedList<String> trainConsist = new LinkedList<>();
+            LinkedHashSet<String> trainFormation = new LinkedHashSet<>();
+
+            trainFormation.add("Engine");
+            trainFormation.add("Sleeper");
+            trainFormation.add("Cargo");
+            trainFormation.add("Guard");
 
 
-            trainConsist.add("Engine");
-            trainConsist.add("Sleeper");
-            trainConsist.add("AC");
-            trainConsist.add("Cargo");
-            trainConsist.add("Guard");
-
-
-            System.out.println();
-            System.out.println("Initial Train Consist:");
-            System.out.println(trainConsist);
-
-            trainConsist.add(2, "Pantry Car");
-
-            System.out.println();
-            System.out.println("After Inserting 'Pantry Car' at position 2:");
-            System.out.println(trainConsist);
-
-
-            trainConsist.removeFirst();
-            trainConsist.removeLast();
-
-            System.out.println();
-            System.out.println("After Removing First and Last Bogie:");
-            System.out.println(trainConsist);
+            trainFormation.add("Sleeper");
 
 
             System.out.println();
-            System.out.println("UC4 ordered consist operations completed...");
+            System.out.println("Train Formation After Insertions:");
+            System.out.println(trainFormation);
+
+
+            System.out.println();
+            System.out.println("UC5 insertion order preserved successfully...");
         }
+
     }
 
 
