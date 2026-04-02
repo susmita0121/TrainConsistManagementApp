2ecf16f9fe78adf1dda9be138921e00bd04be88c
susmita0121 <sushh2123@gmail.com>
2026-04-02 09:19:58 +0530

    UC9

--- App/src/TrainConsistMangementApp.java
+++ App/src/TrainConsistMangementApp.java
@@ -1,43 +1,55 @@
 import java.util.*;
-import java.util.stream.*;
+import java.util.stream.Collectors;
 
 public class TrainConsistMangementApp {
 
 
+    static class Bogie {
         String name;
         int capacity;
 
-        public TrainConsistManagementApp(String name, int capacity) {
+        Bogie(String name, int capacity) {
             this.name = name;
             this.capacity = capacity;
         }
-
-        public int getCapacity() {
-            return capacity;
-        }
-
-        public void display() {
-            System.out.println(name + " - Capacity: " + capacity);
-        }
     }
 
-    public class TrainConsistManagementApp {
-        public static void main(String[] args) {
-            List<Bogie> bogieList = new ArrayList<>();
+    public static void main(String[] args) {
 
-            bogieList.add(new Bogie("Sleeper", 72));
-            bogieList.add(new Bogie("AC Chair", 56));
-            bogieList.add(new Bogie("First Class", 24));
-            bogieList.add(new Bogie("Luxury", 80));
+        System.out.println("========================================");
+        System.out.println("UC9 - Group Bogies by Type");
+        System.out.println("========================================\n");
 
-            List<Bogie> filteredList = bogieList
-                    .stream()
-                    .filter(b -> b.getCapacity() > 60)
-                    .collect(Collectors.toList());
 
-            System.out.println("Filtered Bogies (Capacity > 60):");
-            for (Bogie b : filteredList) {
-                b.display();
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 24));
+        bogies.add(new Bogie("Sleeper", 70));
+        bogies.add(new Bogie("AC Chair", 60));
+
+
+        System.out.println("All Bogies:");
+        for (Bogie b : bogies) {
+            System.out.println(b.name + " -> " + b.capacity);
+        }
+
+
+        Map<String, List<Bogie>> groupedBogies =
+                bogies.stream()
+                        .collect(Collectors.groupingBy(b -> b.name));
+
+
+        System.out.println("\nGrouped Bogies:");
+        for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
+            System.out.println(entry.getKey() + ":");
+
+            for (Bogie b : entry.getValue()) {
+                System.out.println("  " + b.name + " -> " + b.capacity);
             }
         }
+
+        System.out.println("\nUC9 grouping completed...");
     }
+}
