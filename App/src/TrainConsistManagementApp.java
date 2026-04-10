658aca74a4eda510eb2b9aacf4cf46b21e119395
susmita0121 <sushh2123@gmail.com>
2026-04-10 08:52:32 +0530

    UC17:Sorted-Unsorted

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -1,60 +1,14 @@
 
+import java.util.Arrays;
+
 public class TrainConsistManagementApp {
-
-    static class CargoSafetyException extends RuntimeException {
-        public CargoSafetyException(String message) {
-            super(message);
-        }
-    }
-
-    static class GoodsBogie {
-        private String shape;
-        private String cargo;
-
-        public GoodsBogie(String shape) {
-            this.shape = shape;
-        }
-
-        public void assignCargo(String cargoType) {
-            try {
-                if (shape.equalsIgnoreCase("Rectangular") &&
-                        cargoType.equalsIgnoreCase("Petroleum")) {
-                    throw new CargoSafetyException(
-                            "Unsafe Assignment! Petroleum cannot be stored in Rectangular Bogie."
-                    );
-                }
-
-                this.cargo = cargoType;
-                System.out.println("Cargo assigned successfully: " + cargoType);
-
-            } catch (CargoSafetyException e) {
-                System.out.println("Error: " + e.getMessage());
-            } finally {
-                System.out.println("Cargo assignment process completed.\n");
-            }
-        }
-
-        public String getCargo() {
-            return cargo;
-        }
-    }
 
     public static void main(String[] args) {
 
-        GoodsBogie bogie1 = new GoodsBogie("Cylindrical");
-        bogie1.assignCargo("Petroleum");
+        String[] bogieNames = {"Sleeper","AC Chair","First Class","General","Luxury"};
 
-        GoodsBogie bogie2 = new GoodsBogie("Rectangular");
-        bogie2.assignCargo("Petroleum");
+        Arrays.sort(bogieNames);
 
-        GoodsBogie bogie3 = new GoodsBogie("Rectangular");
-        bogie3.assignCargo("Coal");
+        System.out.println(Arrays.toString(bogieNames));
     }
 }
-
-
-
-
-
-
-
